cf4e268d46a9a217cb3d27b18d569b3a7bc7d7e1
Wadadon <35282459+tommycs127@users.noreply.github.com>
2018-11-26 02:02:06 +0800

    Rewrite most code.
    - Used a new algorithm for running loop and checking loops' symmetry;
    - Added a few private functions to improve the readability and efficient of the code;
    - Readability improvement.

--- brainfuck.py
+++ brainfuck.py
@@ -1,55 +1,88 @@
 class Brainfuck:
-
-    def run(self, s):
-        if type(s) is not str: raise TypeError("not str")
+    def __init__(self):
+        self.__clean()
         
-        cell = [0]
-        ptr = 0
-        s_loc = 0 # Location of the string 's'
-        depth = [] # List of while loop command's location
+    '''
+    The parameter
+        "clean" indicates whether cleaning the cell before running the code;
+        "printCell" indicates whether printing the cell after running the code;
+    '''
+    def run(self, s, clean=True, printCell=True):
+        if (type(s) is not str): raise TypeError("{0} is {1}, not str".format(s, type(s)))
+        if (type(clean) is not bool): raise TypeError("{0} is {1}, not bool".format(clean, type(clean)))
+        if (type(printCell) is not bool): raise TypeError("{0} is {1}, not bool".format(printCell, type(printCell)))
         
-        while(s_loc < len(s)):
-            if s[s_loc] == ">":
-                if ptr == len(cell)-1: cell.append(0)
-                ptr += 1
-            elif s[s_loc] == "<":
-                if ptr-1 < 0: raise IndexError("Error @ {0} : index out of range".format(s_loc+1))
-                ptr -= 1
-            elif s[s_loc] == "+":
-                cell[ptr] += 1
-            elif s[s_loc] == "-":
-                cell[ptr] -= 1
-            elif s[s_loc] == ".":
-                print(chr(cell[ptr]), end="")
-            elif s[s_loc] == ",":
-                cell[ptr] = ord((input() or "\0")[0])
-            elif s[s_loc] == "[":
-                temp = depth.copy()
-                depth.append(s_loc)
-                try:
-                    if cell[ptr] == 0:
-                        s_loc += 1
-                        while(temp != depth):
-                            if s[s_loc] == "[":
-                                depth.append(s_loc)
-                            elif s[s_loc] == "]":
-                                if len(depth)-1 == len(temp): s_loc -= 1
-                                del depth[-1]
-                            s_loc += 1
-                except:
-                    raise IndexError("Error @ {0} : loop asymmetry".format(s_loc+1))
-            elif s[s_loc] == "]":
-                try:
-                    if cell[ptr]:
-                        s_loc = depth[-1] - 1
-                    del depth[-1]
-                except:
-                    raise IndexError("Error @ {0} : loop asymmetry".format(s_loc+1))
+        if (clean): self.__clean()
+        
+        s_loc = 0
+        
+        while (s_loc < len(s)):
+            if (s[s_loc] == '['): self.__loopLoc.append([s_loc])
+            elif (s[s_loc] == ']'): self.__insertLoopEnd(s_loc)
             s_loc += 1
         
-        print(", ".join(["[{0}]".format(cell[i]) if i == ptr else "{0}".format(cell[i]) for i in range(len(cell))]))
+        s_loc = 0
         
-try:
-    bf = Brainfuck()
-    bf.run("Your Brainfuck code here")
-except Exception as e: print(e)
+        while (s_loc < len(s)):
+            if (s[s_loc] == '>'):
+                if self.__ptr == len(self.__cell)-1: self.__cell.append(0)
+                self.__ptr += 1
+            elif (s[s_loc] == '<'):
+                if self.__ptr-1 < 0: raise IndexError("Error @ {0} : index out of range".format(s_loc+1))
+                self.__ptr -= 1
+            elif (s[s_loc] == '+'):
+                self.__cell[self.__ptr] += 1
+            elif (s[s_loc] == '-'):
+                self.__cell[self.__ptr] -= 1
+            elif (s[s_loc] == '.'):
+                print(chr(self.__cell[self.__ptr]), end='')
+            elif (s[s_loc] == ','):
+                self.__cell[self.__ptr] = ord((input() or '\0')[0])
+            elif (s[s_loc] == '['):
+                if self.__cell[self.__ptr] == 0: s_loc = self.__findLocation(s_loc)
+            elif (s[s_loc] == ']'):
+                if self.__cell[self.__ptr]: s_loc = self.__findLocation(s_loc)
+            
+            s_loc += 1
+            
+        if (printCell):
+            print("{{ {0} }}".format(
+                    ", ".join(
+                        [
+                            "[{0}]".format(self.__cell[i]) 
+                            if i == self.__ptr else
+                            "{0}".format(self.__cell[i])
+                            for i in range(len(self.__cell))
+                        ]
+                    )
+                )
+            )
+        
+    def __insertLoopEnd(self, loc):
+        if (type(loc) is not int): raise TypeError("{0} is {1}, not int".format(loc, type(loc)))
+        for i in reversed(self.__loopLoc):
+            if (len(i) == 1):
+                i.append(loc)
+                return None
+        raise self.AsymmetryError("Error @ {0}: loop asymmetry".format(loc+1))
+        
+    def __findLocation(self, loc):
+        for i in self.__loopLoc:
+            if (loc in i):
+                other = [item for item in i if item not in [loc]]
+                if (len(other) == 1): return other.pop(0)
+        raise self.AsymmetryError("Error @ {0}: loop asymmetry".format(loc+1))
+        
+    def __clean(self):
+        self.__cell = [0]
+        self.__ptr = 0
+        
+        '''
+        Loop character location.
+        Each element in the list is a list with two integers,
+        indicating the location of '[' and ']'.
+        '''
+        self.__loopLoc = []
+        
+    class AsymmetryError(Exception):
+        pass
