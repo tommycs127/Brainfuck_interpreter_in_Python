f005d03022a48fc0cbb57e1bae8c3c9d48f32e95
Wadadon <35282459+tommycs127@users.noreply.github.com>
2022-05-24 22:16:18 +0800

    Rewrite

--- example.py
+++ example.py
@@ -1,23 +1,12 @@
 import brainfuck
 
-bf = brainfuck.Brainfuck()
+bf = brainfuck.Brainfuck(cells_length=10)
+# A program that prints "Hello world!\n"
+bf.code = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.'
+bf.run()
+bf.print_cells()
 
-'''
-If you don't want to use the prefix "brainfuck.",
-change "import brainfuck" to "from brainfuck import *".
-'''
-
-# Should print "Hello world!" and all cells after running the code
-bf.run("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.")
-print()
-
-# Should print "All cells should not be printed in the next line after running this code."
-bf.run(
-    "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>-----.>++++++++..<<++.>>---------.++.+++++++..+++++++.<<.>>.-----------.+++++++.++++++.---------.--------.<<.>>++++++++++.+.+++++.<<.>>------------------.+++.<<.>>+++++++++++.++.---------.+++++.++++++.---------------.-.<<.>>+++++.+++++.<<.>>++++++.------------.---.<<.>>+++++++++.---------.+++++++++++++++++++.----.<<.>>--------.---.+++++.---------.<<.>>----.+++++.++++++++++++++.---------------.+++++++++++++.<<.>>.+++.-------..-----.+++++.-------.<<.>>+++++++++++++.------------.+.++++++++++.<<.>>----------------.++++++++++++.-----------.+.<<++++++++++++++.",
-    printCell = False
-)
-print()
-
-# Should print "{ [3] }" and then "{ [6] }" because cells are not cleaned before running the second code.
-bf.run("+++")
-bf.run("+++", clean = False)
+bf = brainfuck.Brainfuck(cells_length=10)
+bf.code = '+++++>++<[->+<]]'  # Notice the unmatched parentheses
+bf.run()  # Will show a warning about unmatched parentheses before run
+bf.print_cells()
